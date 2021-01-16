6dbed57e80ead1e599a51ef61f986971f6251565
Berkay Yelekci <mberkay.yelekci@gmail.com>
2021-01-16 13:25:20 +0300

    Added script that contains guessing number with highest and lowest treshold.

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int max = 1000;
+        int min = 1;
+
+        Debug.Log("Welcome to number wizard.");
+        Debug.Log("Pick a number on your mind.");
+        Debug.Log("The highest number you can pick is :" + max);
+        Debug.Log("The lowest number you can pick is : " + min);
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
